907702a372c1ab1098a8a0b34abf216077a2fa91
Nory Pary <Paeypary@gmail.com>
2025-07-11 23:09:40 +0700

    hostingWithreact

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// File: routes/web.php
+
+Route::get('/app/{any}', function () {
+    return file_get_contents(public_path('app/index.html'));
+})->where('any', '.*');
